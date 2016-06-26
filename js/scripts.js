839f93a55dfe8cbdf1b4528be56510a93247c72a
jeremyukim <jeremyukim@gmail.com>
2016-06-26 14:52:05 -0400

    Added functionality to receive user input (First name)

--- js/scripts.js
+++ js/scripts.js
@@ -10,12 +10,23 @@
 //     $('#background').css("background-position", newvalueX + "px     " + newvalueY + "px");
 //   });
 // });
+
+var userName = "";
+//Get user's name
+function setName(e) {
+    if(event.keyCode == 13) {
+        userName = document.getElementById("userName").value;
+        updateTime();
+    }
+}
+
+//Update displayed time and greeting
 function updateTime(){
   var today = new Date();
   var time = today.getHours();
   var minute = today.getMinutes();
 
-  //For displaying the current time
+  //Sets the current time
   var hour = time;
   if (hour === 0) {
     hour = 12;
@@ -26,27 +37,44 @@ function updateTime(){
   if (minute < 10) {
     minute = "0" + minute;
   }
+
+  //Sets the greeting
   $(".currentHour").text(hour + ":" + minute);
-}
-
-$(document).ready(function(){
-  var today = new Date();
-  var time = today.getHours();
-  var minute = today.getMinutes();
-
-  setInterval(updateTime, 1000);
-
   if (time < 3 || time > 22) {
     //Change greeting to: It's getting late, <name>. Try to get some rest!
-    $(".greeting").text("It's getting late, Jeremy. Try to get some rest!");
+    $(".greeting").text("Good evening, " + userName);
+    $(".motivation").text("It's getting late. Try to get some rest!");
   } else if (time < 12) {
     //Change greeting to: Good morning, <name>. It's a brand new day, make it a great one! :)
-    $(".greeting").text("Good morning, Jeremy. It's a brand new day, make it a great one! :)");
+    $(".greeting").text("Good morning, _______.");
+    $(".motivation").text("It's a brand new day, make it a great one!");
   } else if (time < 16) {
     //Change greeting to: Good afternoon, <name>.
-    $(".greeting").text("Good afternoon, Jeremy.");
+    $(".greeting").text("Good afternoon, " + userName);
+    $(".motivation").text("Hmmm what should you eat for lunch?");
   } else if (time < 23) {
     //Change greeting to: Good evening, <name>.
-    $(".greeting").text("Good evening, Jeremy.");
+    $(".greeting").text("Good evening, _______.");
+  }
+}
+
+    // Weather.getCurrent( "Toronto", function( current ) {
+    //     $( "#current" ).html( "Temperature: " +
+    //         Weather.kelvinToFahrenheit( current.temperature() ) + "&deg;F or " +
+    //         Weather.kelvinToCelsius( current.temperature() ) + "&deg;C" +
+    //         "<br />Current Conditions: " + current.conditions() );
+    // });
+    //
+    // Weather.getForecast( "Toronto", function ( forecast ) {
+    //     $( "#forecast" ).html( "High: " +
+    //         Weather.kelvinToFahrenheit( forecast.high() ) + "&deg;F or " +
+    //         Weather.kelvinToCelsius( forecast.high() ) + "&deg;C<br />Low: " +
+    //         Weather.kelvinToFahrenheit( forecast.low() ) + "&deg;F or " +
+    //         Weather.kelvinToCelsius( forecast.low() ) + "&deg;C" );
+    // });
+
+$(document).ready(function(){
+  if(userName!=""){
+    setInterval(updateTime, 1000);
   }
 });
